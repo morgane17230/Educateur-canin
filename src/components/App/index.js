caa2b561217a53f0b5ba1c77fb6262d0f6671bb2
morgane17230 <aboumorgane@live.fr>
2021-08-01 06:37:06 +0200

    datepicker

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from '../Calendar/index';
+import 'src/styles/index.scss';
+// == Composant
+const App = () => (
+  <div className="app">
+    <Calendar />
+  </div>
+);
+
+// == Export
+export default App;
