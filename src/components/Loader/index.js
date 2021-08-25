715c876303897666a207d8c1008305609c1a08b8
morgane17230 <aboumorgane@live.fr>
2021-08-25 06:44:33 +0200

    favicon

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -5,13 +5,15 @@ import 'src/styles/loader.scss';
 const Loader = () => {
   console.log('loader');
   return (
-    <div className="paw-print">
-      <div className="pad large" />
-      <div className="pad small-1" />
-      <div className="pad small-2" />
-      <div className="pad small-3" />
-      <div className="pad small-4" />
-    </div>
+    <>
+      <div className="paw-print">
+        <div className="pad large" />
+        <div className="pad small-1" />
+        <div className="pad small-2" />
+        <div className="pad small-3" />
+        <div className="pad small-4" />
+      </div>
+    </>
   );
 };
 
